2f7f003ec3fd2ab583def7fba0df22bccfe5f497
Yishai Oltchik <yishai.oltchik@mail.huji.ac.il>
2014-06-01 11:38:30 +0300

    Starting flow commands, currently without VM command generation

--- CodeWriter.py
+++ CodeWriter.py
@@ -3,13 +3,72 @@ __author__ = 'larrath'
 from SymbolTable import ScopeChain
 import xml.dom.minidom as minidom
 
+def expect_label(xml_data, desired_value):
+    if (xml_data.tagName.strip() != desired_value):
+        raise Exception('Label should be ' + str(desired_value) + '; instead got ' + xml_data.tagName)
+
+def expect_values(xml_data, desired_values):
+    if (str(xml_data.firstChild.nodeValue).strip() not in desired_values):
+        raise Exception('Label should be in' + str(desired_values) + '; instead got ' + xml_data.firstChild.nodeValue)
+
 def encode(xml_data):
+    expect_label(xml_data, 'class')
+
+    # parsing new class
     scope = ScopeChain()
-    scope_chain = []
-    static_vars = []
-    print (xml_data.toprettyxml())
+    main_nodes = list(xml_data.childNodes)
+
+    compileClassDeclaration(xml_data, scope, '')
     print("encode exit")
     exit()
+
+def compileClassDeclaration(xml_data, scope, buf):
+    class_data = list(xml_data.childNodes)
+    class_name = class_data[1].firstChild.nodeValue
+    class_body = class_data[3]
+    
+    print('Recognized Class Declaration')
+    print('\tClass name:' + str(class_name))
+    print('\tCompiling class body')
+    compileClassBody(class_body, scope, buf)
+
+def compileClassBody(xml_data, scope, buf):
+    expect_label(xml_data, 'subroutineDec')
+    print('Recognized class body')
+    
+    while (xml_data.hasChildNodes()):
+        print('body data length: ' + str(len(xml_data.childNodes)))
+        current_top = xml_data.childNodes[0]
+        print(current_top.firstChild.nodeValue)
+        if (str(current_top.firstChild.nodeValue).strip() in ['constructor', 'function', 'method']):
+            compileFunctionDeclaration(current_top.parentNode, scope, buf)
+    
+    for node in xml_data.childNodes:
+        print node.tagName + ' : ' + str(node.firstChild.nodeValue)
+
+def compileFunctionDeclaration(xml_data, scope, buf):
+    expect_values(xml_data.firstChild, ['constructor', 'function', 'method'])
+    print("Recognized function declaration")
+    func_data = list(xml_data.childNodes)
+    for node in func_data:
+        print(node)
+    
+    function_declare_mode = func_data[0].firstChild.nodeValue.strip()
+    function_return_type = func_data[1].firstChild.nodeValue.strip()
+    function_name = func_data[2].firstChild.nodeValue.strip()
+    function_args = func_data[4] # parse parameter list
+    function_body = func_data[6] # parse function body
+    print('Function data: (' + function_declare_mode + ') ' + function_return_type + ' ' + function_name)
+    # TODO: function names are not added to scope, right?
+
+    compileFunctionArguments(function_args, scope, buf)
+    compileFunctionBody(function_body, scope, buf)
+
+    # destroy the nodes we compiled
+    xml_data.parentNode.removeChild(xml_data)
+    xml_data.unlink()
+
+
 
 def writePush(mem_segment, index):
     pass # TODO: writes a VM push command
---
+++ Makefile
@@ -0,0 +1,38 @@
+tiny:
+	python JackCompiler.py Seven
+
+testSeven:
+	@rm -f sevenOutput.log
+	python JackCompiler.py Seven > sevenOutput.log
+
+testPong:
+	@rm -f pongOutput.log
+	python JackCompiler.py Pong > pongOutput.log
+
+testAverage:
+	@rm -f averageOutput.log
+	python JackCompiler.py Average > averageOutput.log
+
+testSquare:
+	@rm -f squareOutput.log
+	python JackCompiler.py Square > squareOutput.log
+
+testComplexArrays:
+	@rm -f complexArraysOutput.log
+	python JackCompiler.py ComplexArrays > complexArraysOutput.log
+
+testConvertToBin:
+	@rm -f convertToBinOutput.log
+	python JackCompiler.py ConvertToBin > convertToBinOutput.log
+
+test: testSeven testPong testAverage testSquare testComplexArrays testConvertToBin
+
+clean:
+	@rm -f sevenOutput.log
+	@rm -f pongOutput.log
+	@rm -f averageOutput.log
+	@rm -f SquareOutput.log
+	@rm -f complexArraysOutput.log
+	@rm -f convertToBinOutput.log
+
+.PHONY: clean testSeven testPong testAverage testSquare testComplexArrays testConvertToBin test tiny
